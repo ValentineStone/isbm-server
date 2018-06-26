18add6572f689f0b78fb55489832a59be3023327
Valentine Stone <sirValentineStone@gmail.com>
2018-06-26 07:38:58 +0500

    before performing

--- jsonrpcEndpoint/addRecord.js
+++ jsonrpcEndpoint/addRecord.js
@@ -1,19 +1,20 @@
 const recordTypes = require('../recordTypes')
 const pickProps = require('lodash/pick')
 
-module.exports = function setRecord(type) {
+module.exports = function addRecord(type, record = {}) {
   if (!this.user)
     throw new Error('No user session present')
 
   if (!recordTypes.includes(type))
     throw new Error('Unknown record type: ' + type)
 
-  return this.db.querySingle(
-    `CREATE (record:${type} {
-        id: apoc.create.uuid(),
-        timeCreated: $timeCreated,
-        timeEdited: $timeCreated
-    }) RETURN record {.*}`,
-    {timeCreated: Date.now()}
+  return this.db.querySingle(`
+    CREATE (record:${type})
+    SET record = $record
+    SET record.id = apoc.create.uuid()
+    SET record.timeCreated = $timeCreated
+    SET record.timeEdited = $timeCreated
+    RETURN record {.*}`,
+    { timeCreated: Date.now(), record }
   ).then(v => v.record)
 }
